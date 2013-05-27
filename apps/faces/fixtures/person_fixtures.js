c9fc38810badf424f88f94a7071cc19c7f8df485
aoifegrey <aoifegrey@gmail.com>
2013-05-27 16:37:29 +0100

    First Commit. Added faces project with sproutcore gen project faces command

---
+++ apps/faces/fixtures/person_fixtures.js
@@ -0,0 +1,26 @@
+// ==========================================================================
+// Project:   Faces.Person Fixtures
+// Copyright: @2013 My Company, Inc.
+// ==========================================================================
+/*globals Faces */
+
+sc_require('models/person_model');
+
+Faces.Person.FIXTURES = [
+
+{
+    "guid" : "person-1",
+    "name" : "Brian O'Driscoll",
+    "company" : "Leinster",
+    "website" : "http://www.leinsterrugby.ie/index.php",
+    "picture" : "http://cdn.soticservers.net/tools/images/players/photos/2011/irfu/126/295x300/4038.jpg" 
+},
+{
+    "guid" : "person-2",
+    "name" : "Tommy Bowe",
+    "company" : "Ulster",
+    "website" : "http://www.ulsterrugby.com/",
+    "picture" : "http://cdn.soticservers.net/tools/images/players/photos/2011/irfu/126/295x300/4922.jpg" 
+}
+
+];
